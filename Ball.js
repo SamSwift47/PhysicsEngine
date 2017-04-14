44c0f6f76ec467e85ebe9a4564888377c0569fd9
SamSwift47 <s.e.swift47@gmail.com>
2017-04-13 22:36:46 -0400

    This is a start. It is ok but not great

--- Ball.js
+++ Ball.js
@@ -1,5 +1,5 @@
 class ball{
-	constructor(x, y, r, xspeed, yspeed, m, elasticity, drag, rollingResistance, colour, walls, movable){//Change to use magnitue and direction as it much easier
+	constructor(x, y, r, xspeed, yspeed, m, elasticity, k, drag, rollingResistance, colour, walls, movable){//Change to use magnitue and direction as it much easier
 		this.x = x;
 		this.y = y;
 		this.r = r;
@@ -7,6 +7,7 @@ class ball{
 		this.yspeed = yspeed;
 		this.m = m;
 		this.elasticity = elasticity;
+		this.k = k;
 		this.drag = drag;
 		this.rollingResistance = rollingResistance;
 		this.colour = colour;
@@ -103,54 +104,12 @@ class ball{
 		console.log("this.m: " + this.m);
 		console.log("object.m: " + object.m);
 
-		//caclulate v2 paralell and perpindicular
-		//this.m * Va1ll + object.m * Vbll = this.m * Va2ll + object.m * Vb2ll;
-		//Va2ll = (this.m * Va1ll + object.m * Vb1ll - object.m * Vb2ll) / this.m; //Va2ll in terms of Vb2ll
-		//e initial * energy loss = e final
-		//(this.m * (Math.pow(Va1ll, 2) + Math.pow(Va1X, 2)) + object.m * (Math.pow(Vb1ll, 2) + Math.pow(Vb1X, 2)) * this.elasticity * object.elasticity (INCORPERATE THE MAXIMUM AMOUNT OF ENERGY THAT COULD BE LOST IF IT WAS A PERFECTLY INELASTIC COLISIIN) = this.m * (Math.pow(Va2ll, 2) + Math.pow(Va2X, 2)) + object.m * (Math.pow(Vb2ll, 2) + Math.pow(Vb2X, 2))//Could also just x and y to get the magnitude of velocity
 
-		//(this.m * (Math.pow(Va1ll, 2) + Math.pow(Va1X, 2)) + object.m * (Math.pow(Vb1ll, 2) + Math.pow(Vb1X, 2)) = this.m * (Math.pow(Va2ll, 2) + Math.pow(Va2X, 2)) + object.m * (Math.pow(Vb2ll, 2) + Math.pow(Vb2X, 2))
-		if(this.movable && object.movable){
-			var a = (object.m + Math.pow(object.m, 2) / this.m);
-			var b = -(2 * Va1ll * object.m + 2 * Math.pow(object.m, 2) * Vb1ll / this.m)
-			var c = -this.m * (Math.pow(Va1ll, 2) + Math.pow(Va1X, 2)) - object.m * (Math.pow(Vb1ll, 2) + Math.pow(Vb1X, 2)) + object.m * Math.pow(Vb2X, 2) + this.m * Math.pow(Va1ll, 2) + 2 * object.m * Vb1ll * Va1ll + Math.pow(object.m, 2) * Math.pow(Vb1ll, 2) / this.m;
-
-			console.log("B a: " + a);
-			console.log("B b: " + b);
-			console.log("B c: " + c);
-
-			var Vb2ll = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-			console.log("Vb2ll: " + Vb2ll);
-			if(Vb2ll < Vb1ll + error && Vb2ll > Vb1ll - error){
-				Vb2ll = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-				console.log("HAS FOUND AN ERROR");
-				console.log("Vb2ll: " + Vb2ll);
-			}
-
-			a = (this.m + Math.pow(this.m, 2) / object.m);
-			b = -(2 * Vb1ll * this.m + 2 * Math.pow(this.m, 2) * Va1ll / object.m)
-			c = -object.m * (Math.pow(Vb1ll, 2) + Math.pow(Vb1X, 2)) - this.m * (Math.pow(Va1ll, 2) + Math.pow(Va1X, 2)) + this.m * Math.pow(Va2X, 2) + object.m * Math.pow(Vb1ll, 2) + 2 * this.m * Va1ll * Vb1ll + Math.pow(this.m, 2) * Math.pow(Va1ll, 2) / object.m;
-
-			console.log("A a: " + a);
-			console.log("A b: " + b);
-			console.log("A c: " + c);
-
-			var Va2ll = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-			console.log("Va2ll: " + Va2ll);
-			if(Va2ll < Va1ll + error && Va2ll > Va1ll - error){
-				Va2ll = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-				console.log("HAS FOUND AN ERROR");
-				console.log("Va2ll: " + Va2ll);
-			}
+		if(this.movable){
+			Va2ll = Va1ll - ((this.k * object.k * (this.r + object.r - Math.sqrt(Math.pow((object.y - this.y), 2) + Math.pow((object.x - this.x), 2)))) / (this.k + object.k)) / this.m * t * 100000;
 		}
-		else if(this.movable){
-			Va2ll = -Va1ll;
-			console.log("Va2ll: " + Va2ll);
-		}
-		else if(object.movable){
-			Vb2ll = -Vb1ll;
-			console.log("Vb2ll: " + Vb2ll);
-		}
+
+
 		this.xspeed = Math.cos(-theta) * Va2ll + Math.sin(-theta) * Va2X;
 		this.yspeed = -Math.sin(-theta) * Va2ll + Math.cos(-theta) * Va2X;
 		object.xspeed = Math.cos(-theta) * Vb2ll + Math.sin(-theta) * Vb2X;
